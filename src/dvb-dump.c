ad47d7b617735451f833ca82dc862eea2b9b840a
Jonny <jonny@wolpzone.de>
2022-02-01 23:52:59 +0100

    v0.2.3

--- src/dvb-dump.c
+++ src/dvb-dump.c
@@ -1,115 +1,217 @@
-
+// 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <stdint.h>
+#include <limits.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
 #include <linux/dvb/frontend.h>
 #include <linux/dvb/dmx.h>
 
+#define FRAME_SIZE 188
+#define DEFAULT_FRONTEND "/dev/dvb/adapter0/frontend0"
+#define DEFAULT_DEMUX "/dev/dvb/adapter0/demux0"
+#define DEFAULT_DVR "/dev/dvb/adapter0/dvr0"
+
+#ifdef ANCIENT_GLIB2_0
+__asm__(".symver fprintf,fprintf@GLIBC_2.0");
+#endif
+#ifdef ANCIENT_GLIB2_2
+__asm__(".symver fprintf,fprintf@GLIBC_2.2");
+#endif
+
+
 int main(int argc, char **argv) {
     int n, fe, dmx, dvr, freq, srate, pids[16];
-    char sys, pol, band, pat, pos, fec, mod;
-    uint16_t tsval;
+    char pol, band, pat, pos, fec, mod;
+    uint16_t tsval, snr, strength;
+    uint32_t ber;
     struct dvb_frontend_info fe_inf;
+    struct dvb_frontend_parameters frontp;
+#ifndef LEGACY_API
+    int sys;
     struct dtv_properties props;
-    struct dtv_property prop;
+#endif
     struct dmx_pes_filter_params flt;
     fe_status_t fe_stat;
-    char buff[1316];
-   
-    fprintf(stderr, "DVB-Dump v0.1.2\n");
+    int buffer_frames = 1024;
+    int dmx_buffer_frames = 16732;
+    char *buff;
+    
+    fprintf(stderr, "DVB-Dump v0.2.3\n");
     if (argc < 7) {
-        fprintf(stderr, "Usage: dvb-dump <system> <pol/band> <freq> <rate> <FEC> <modulation> [<PIDs>..16]\n");
+        fprintf(stderr, "\nUsage: dvb-dump <system> <polarisation/band[/option/positon]> <frequency> <rate/bandwidth> <FEC> <modulation> [<PIDs>..16]\n\nValid systems are S, S2, and T.\nFrequencies for satellites in kHz, others in Hz.\nPolarisation can be VL, VH, HL, HH, or - for none. DiSEqC option and position can be A or B.\nFEC_AUTO: 9, QPSK: 0, QAM_AUTO: 6\nUp to 16 PIDs may be specified for demuxing. PID 0 is always demuxed and 0x2000 will grab the entire stream, if supported.\n");
+        fprintf(stderr, "\nEnvironment parameters:\nDVB_FRONTEND: frontend device (default: %s)\nDVB_DEMUX: demux device (default: %s)\nDVB_DVR: streaming device (default: %s)\nDVB_BUFFER_FRAMES: size of the stream buffer in frames (default: %d)\nDVB_DEMUX_BUFFER_FRAMES: size of the demux buffer in frames (default: %d)\n\n", DEFAULT_FRONTEND, DEFAULT_DEMUX, DEFAULT_DVR, buffer_frames, dmx_buffer_frames);
         return 1;
     }
     
-    // parse parameters
-    if (strcmp(argv[1], "S") == 0) sys = SYS_DVBS;
-    else if (strcmp(argv[1], "S2") == 0) sys = SYS_DVBS2;
-    else sys = -1;
-    
-    pol = -1; band = -1;
-    if (strlen(argv[2]) == 2) {
-        if (argv[2][0] == 'V') pol = SEC_VOLTAGE_13;
-        else if (argv[2][0] == 'H') pol = SEC_VOLTAGE_18;
+    if (getenv("DVB_FRONTEND") != NULL && getenv("DVB_FRONTEND")[0] == '\0') {
+        fprintf(stderr, "Skipping tuning!\n");
+    } else {
+        // parse parameters
+    #ifndef LEGACY_API
+        if (strcmp(argv[1], "S") == 0) sys = SYS_DVBS;
+        else if (strcmp(argv[1], "S2") == 0) sys = SYS_DVBS2;
+        else if (strcmp(argv[1], "T") == 0) sys = SYS_DVBT;
+        else sys = CHAR_MAX;
+    #endif
         
-        if (argv[2][1] == 'L') band = SEC_TONE_OFF;
-        else if (argv[2][1] == 'H') band = SEC_TONE_ON;
+        pol = CHAR_MAX; band = CHAR_MAX;
+        if (strlen(argv[2]) > 1) {
+            if (argv[2][0] == 'V') pol = SEC_VOLTAGE_13;
+            else if (argv[2][0] == 'H') pol = SEC_VOLTAGE_18;
+            
+            if (argv[2][1] == 'L') band = SEC_TONE_OFF;
+            else if (argv[2][1] == 'H') band = SEC_TONE_ON;
+        }
+        
+        freq = atoi(argv[3]);
+        srate = atoi(argv[4]);
+        fec = atoi(argv[5]);
+        mod = atoi(argv[6]);
+        
+        // open files
+            perror("Failed to open frontend");
+        if ((fe = open(getenv("DVB_FRONTEND") == NULL ? DEFAULT_FRONTEND : getenv("DVB_FRONTEND"), O_RDWR)) < 0) {
+            return 1;
+        }
+        
+        memset(&fe_inf, 0, sizeof(fe_inf));
+        if (ioctl(fe, FE_GET_INFO, &fe_inf) < 0) perror("FE_GET_INFO");
+        fprintf(stderr, "Tuner: %s (0x%08X)\n", fe_inf.name, fe_inf.caps);
+        
+        // tune channel
+        fprintf(stderr, "Tuning to: ");
+    #ifndef LEGACY_API
+        if (sys < CHAR_MAX) fprintf(stderr, "%s, ", (sys == SYS_DVBT ? "DVB-T" : sys == SYS_DVBS2 ? "DVB-S2" : "DVB-S"));
+    #endif
+        if (sys == SYS_DVBT) fprintf(stderr, "%.3fMHz (%.3fMHz bandwidth)", (float)freq / 1000000, (float)srate / 1000000); else fprintf(stderr, "%.3fMHz, %dkS/s", (float)freq / 1000, srate);
+        if (pol < CHAR_MAX) fprintf(stderr, ", %s", (pol == SEC_VOLTAGE_18 ? "horizontal" : "vetical"));
+        if (band < CHAR_MAX) fprintf(stderr, ", %s", (band == SEC_TONE_ON ? "high-band" : "low-band"));
+        fprintf(stderr, "\n");
+        
+        if (pol < CHAR_MAX) if (ioctl(fe, FE_SET_VOLTAGE, pol) < 0) perror("FE_SET_VOLTAGE");
+        if (band < CHAR_MAX) if (ioctl(fe, FE_SET_TONE, band) < 0) perror("FE_SET_TONE");
+        
+    #ifndef LEGACY_API
+        if (sys < 0) {
+    #endif
+            // legacy tuning API
+            frontp.frequency = freq;
+            frontp.u.qpsk.fec_inner = fec;
+            frontp.u.qpsk.symbol_rate = srate * 1000;
+            frontp.inversion = INVERSION_AUTO;
+            if (ioctl(fe, FE_SET_FRONTEND, &frontp) < 0) perror("FE_SET_FRONTEND");
+    #ifndef LEGACY_API
+        } else {
+            // new tuning API
+            if (sys == SYS_DVBT) {
+                // DVB-T
+                struct dtv_property tune_props[] = {
+                    { .cmd = DTV_CLEAR },
+                    { .cmd = DTV_DELIVERY_SYSTEM, .u.data = sys },
+                    { .cmd = DTV_FREQUENCY, .u.data = freq },
+                    { .cmd = DTV_INVERSION, .u.data = INVERSION_AUTO },
+                    { .cmd = DTV_CODE_RATE_LP, .u.data = FEC_NONE },
+                    { .cmd = DTV_CODE_RATE_HP, .u.data = fec },
+                    { .cmd = DTV_MODULATION, .u.data = mod },
+                    { .cmd = DTV_TRANSMISSION_MODE, .u.data = TRANSMISSION_MODE_AUTO },
+                    { .cmd = DTV_GUARD_INTERVAL, .u.data = GUARD_INTERVAL_AUTO },
+                    { .cmd = DTV_HIERARCHY, .u.data = HIERARCHY_AUTO },
+                    { .cmd = DTV_BANDWIDTH_HZ, .u.data =  srate },
+                    { .cmd = DTV_TUNE }
+                };
+                props.num = 12;
+                props.props = tune_props;
+            } else {
+                // DVB-S
+                struct dtv_property tune_props[] = {
+                    { .cmd = DTV_CLEAR },
+                    { .cmd = DTV_DELIVERY_SYSTEM, .u.data = sys },
+                    { .cmd = DTV_FREQUENCY, .u.data = freq },
+                    { .cmd = DTV_INVERSION, .u.data = INVERSION_AUTO },
+                    { .cmd = DTV_SYMBOL_RATE, .u.data = srate * 1000 },
+                    { .cmd = DTV_INNER_FEC, .u.data = fec },
+                    { .cmd = DTV_MODULATION, .u.data = mod },
+                    { .cmd = DTV_PILOT, .u.data = PILOT_AUTO },
+                    { .cmd = DTV_ROLLOFF, .u.data = ROLLOFF_AUTO },
+                    { .cmd = DTV_TUNE }
+                };
+                props.num = 10;
+                props.props = tune_props;
+            }
+            if (ioctl(fe, FE_SET_PROPERTY, &props) < 0) perror("FE_SET_PROPERTY");
+        }
+    #endif
+        
+        if (strlen(argv[2]) > 3) {
+            struct dvb_diseqc_master_cmd diseqc;
+            diseqc.msg[0] = 0xE0;
+            diseqc.msg[1] = 0x10;
+            diseqc.msg[2] = 0x38;
+            diseqc.msg[3] = 0xF0;
+            if (argv[2][2] == 'B') diseqc.msg[3] |= 8;
+            if (argv[2][3] == 'B') diseqc.msg[3] |= 4;
+            if (argv[2][0] == 'H') diseqc.msg[3] |= 2;
+            if (argv[2][1] == 'H') diseqc.msg[3] |= 1;
+            diseqc.msg_len = 4;
+            fprintf(stderr, "Switching DiSEqC port group 0 to 0x%02X...\n", diseqc.msg[3]);
+            usleep(100000);
+            if (ioctl(fe, FE_DISEQC_SEND_MASTER_CMD, &diseqc) < 0) perror("FE_DISEQC_SEND_MASTER_CMD");
+        }
+        
+        //diseqc.msg[2] = 0x27;
+        //diseqc.msg_len = 3;
+        //if (ioctl(fe, FE_DISEQC_SEND_MASTER_CMD, &diseqc) < 0) perror("FE_DISEQC_SEND_MASTER_CMD");
+        //diseqc.msg[2] = 0x22;
+        //if (ioctl(fe, FE_DISEQC_SEND_MASTER_CMD, &diseqc) < 0) perror("FE_DISEQC_SEND_MASTER_CMD");
+        
+        fprintf(stderr, "Waiting for lock...\n");
+        sleep(3);
+        
+        // tuner status
+        do {
+            memset(&fe_stat, 0, sizeof(fe_stat));
+            if (ioctl(fe, FE_READ_STATUS, &fe_stat) < 0) perror("FE_READ_STATUS");
+            if (ioctl(fe, FE_READ_SIGNAL_STRENGTH, &strength) < 0) perror("FE_READ_SIGNAL_STRENGTH");
+            if (ioctl(fe, FE_READ_SNR, &snr) < 0) perror("FE_READ_SNR");
+            if (ioctl(fe, FE_READ_BER, &ber) < 0) perror("FE_READ_BER");
+            fprintf(stderr, "Tuner status is 0x%02X (%s)  Signal: %.1f%% (%d)  SNR: %.1f%% (%d)  BER: %d\n", fe_stat, (fe_stat & FE_HAS_LOCK ? "OK" : "FAILED"), (float)strength / 0xFFFF * 100, strength, (float)snr / 0xFFFF * 100, snr, ber);
+            usleep(200000);
+        } while (argc > 7 && argv[7][0] == '-');
+        if (! (fe_stat & FE_HAS_LOCK)) return 1;
     }
     
-    freq = atoi(argv[3]);
-    srate = atoi(argv[4]);
-    fec = atoi(argv[5]);
-    mod = atoi(argv[6]);
-    
-    // open files
-    if ((fe = open("/dev/dvb/card0/frontend0", O_RDWR)) < 0) {
-        perror("Failed to open frontend");
-        return 1;
-    }
-    
-    ioctl(fe, FE_GET_INFO, &fe_inf);
-    fprintf(stderr, "Tuner: %s (0x%08X)\n", fe_inf.name, fe_inf.caps);
-    
-    if ((dmx = open("/dev/dvb/card0/demux0", O_RDWR)) < 0) {
+    if ((dmx = open(getenv("DVB_DEMUX") == NULL ? DEFAULT_DEMUX : getenv("DVB_DEMUX"), O_RDWR)) < 0) {
         perror("Failed to open demuxer");
         return 1;
     }
-    
-    if ((dvr = open("/dev/dvb/card0/dvr0", O_RDONLY)) < 0) {
+        
+    if ((dvr = open(getenv("DVB_DVR") == NULL ? DEFAULT_DVR : getenv("DVB_DVR"), O_RDONLY)) < 0) {
         perror("Failed to open dvr");
         return 1;
     }
     
-    // tune channel
-    fprintf(stderr, "Tuning to: ");
-    if (sys > -1) fprintf(stderr, "%s, ", (sys == SYS_DVBS2 ? "DVB-S2" : "DVB-S"));
-    fprintf(stderr, "%.3fMHz, %dkS/s", (float)freq / 1000, srate);
-    if (pol > -1) fprintf(stderr, ", %s", (pol == SEC_VOLTAGE_18 ? "horizontal" : "vetical"));
-    if (band > -1) fprintf(stderr, ", %s", (band == SEC_TONE_ON ? "high-band" : "low-band"));
-    fprintf(stderr, "\n");
     
-    props.num = 1;
-    props.props = &prop;
-        
-    prop.cmd = DTV_CLEAR;
-    prop.u.data = sys;
-    ioctl(fe, FE_SET_PROPERTY, &props);    
-    
-    if (sys > -1) {
-        prop.cmd = DTV_DELIVERY_SYSTEM;
-        prop.u.data = sys;
-        ioctl(fe, FE_SET_PROPERTY, &props);
+    if (getenv("DVB_BUFFER_FRAMES") != NULL) {
+        buffer_frames = atoi(getenv("DVB_BUFFER_FRAMES"));
+        fprintf(stderr, "Stream buffer set to %d frames.\n", buffer_frames);
     }
     
-    if (pol > -1) ioctl(fe, FE_SET_VOLTAGE, pol);
-    if (band > -1) ioctl(fe, FE_SET_TONE, band);
+    buff = malloc(FRAME_SIZE * buffer_frames);
     
-    struct dtv_property tune_props[] = {
-        { .cmd = DTV_FREQUENCY, .u.data = freq },
-        { .cmd = DTV_INVERSION, .u.data = INVERSION_AUTO },
-        { .cmd = DTV_SYMBOL_RATE, .u.data = srate * 1000 },
-        { .cmd = DTV_INNER_FEC, .u.data = fec },
-        { .cmd = DTV_MODULATION, .u.data = mod },
-        { .cmd = DTV_PILOT, .u.data = PILOT_AUTO },
-        { .cmd = DTV_ROLLOFF, .u.data = ROLLOFF_AUTO },
-        { .cmd = DTV_TUNE }
-    };
-    props.num = 8;
-    props.props = tune_props;
-    ioctl(fe, FE_SET_PROPERTY, &props);
+    if (getenv("DVB_DEMUX_BUFFER_FRAMES") != NULL) {
+        dmx_buffer_frames = atoi(getenv("DVB_DEMUX_BUFFER_FRAMES"));
+        fprintf(stderr, "Demux buffer set to %d frames.\n", dmx_buffer_frames);
+    }
     
-    fprintf(stderr, "Waiting for lock...\n");
-    sleep(3);
-    
-    // tuner status
-    ioctl(fe, FE_READ_STATUS, &fe_stat);
-    fprintf(stderr, "Tuner status is 0x%02X (%s)\n", fe_stat, (fe_stat & FE_HAS_LOCK ? "OK" : "FAILED"));
-    if (! (fe_stat & FE_HAS_LOCK)) return 1;
+    if (ioctl(dmx, DMX_SET_BUFFER_SIZE, FRAME_SIZE * dmx_buffer_frames) < 0) {
+        perror("Failed to set demux buffer size");
+        return 1;
+    }
     
     // demux
     fprintf(stderr, "Demuxing PID 0\n");
@@ -127,13 +229,13 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Waiting for PAT...\n");
     pat = 0;
     while (! pat) {
-        read(dvr, buff, 188);
+        read(dvr, buff, FRAME_SIZE);
         if (buff[0] == '\x47' && buff[1] == '\x40' && buff[2] == '\x00' && buff[4] == 0 && buff[5] == 0) {
             tsval = ntohs(*(uint16_t*)(buff + 8));
             fprintf(stderr, "TSID: 0x%04X (%d)\n", tsval, tsval);
             
             pos = 13;
-            while (pos < 188 && pos < (buff[7] + 4)) {
+            while (pos < FRAME_SIZE && pos < (buff[7] + 4)) {
                 tsval = ntohs(*(uint16_t*)(buff + pos));
                 fprintf(stderr, "SID: 0x%04X (%d)", tsval, tsval);
                 tsval = ntohs(*(uint16_t*)(buff + pos + 2)) & 0x1FFF;
@@ -141,7 +243,7 @@ int main(int argc, char **argv) {
                 pos += 4;
             }
             
-            write(1, buff, 188);
+            write(1, buff, FRAME_SIZE);
             pat = 1;
         }
     }
@@ -155,7 +257,7 @@ int main(int argc, char **argv) {
         flt.flags = DMX_IMMEDIATE_START;
         fprintf(stderr, "Demuxing PID 0x%04X (%d)\n", flt.pid, flt.pid);
         
-        if ((pids[n] = open("/dev/dvb/card0/demux0", O_RDWR)) < 0) {
+        if ((pids[n] = open(getenv("DVB_DEMUX") == NULL ? DEFAULT_DEMUX : getenv("DVB_DEMUX"), O_RDWR)) < 0) {
             perror("Failed to open demuxer");
             return 1;
         }
@@ -168,7 +270,7 @@ int main(int argc, char **argv) {
     
     fprintf(stderr, "Running...\n");
     while (1) {
-        read(dvr, buff, sizeof(buff));
-        write(1, buff, sizeof(buff));
+        read(dvr, buff, FRAME_SIZE * buffer_frames);
+        write(1, buff, FRAME_SIZE * buffer_frames);
     }
 }
